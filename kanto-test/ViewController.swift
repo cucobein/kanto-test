67b638442a57cd87ae6465481c460cac25bceafd
Hugo Ramirez <cucobein@gmail.com>
2021-03-16 16:58:08 -0600

    Initial Commit

---
+++ kanto-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kanto-test
+//
+//  Created by Hugo Jovan Ramírez Cerón on 16/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
